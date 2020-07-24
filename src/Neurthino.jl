e176daef40d84d5e86895334b1a3b96d03f0a1ba
Johannes Schumann <johannes.schumann@fau.de>
2020-07-24 11:40:32 +0200

    Temporary add of the fermi constant

--- src/Neurthino.jl
+++ src/Neurthino.jl
@@ -2,8 +2,16 @@ module Neurthino
 
 using LinearAlgebra
 using SparseArrays
+using PhysicalConstants
+using Unitful
 
 import Polynomials: Poly
+
+# TEMPORARY UNTIL PhysicalConstants.jl GETS UPDATED
+G_F = 1.1663787e-5u"GeV^-2"
+G_F = G_F * (PhysicalConstants.CODATA2018.SpeedOfLightInVacuum * PhysicalConstants.CODATA2018.ReducedPlanckConstant)^3
+G_F = uconvert(u"eV*cm^3", G_F)
+
 
 include("PREM.jl")
 include("Oscillation.jl")
