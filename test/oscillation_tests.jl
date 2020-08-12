9e3fa255ae9a194512f064f7a8f561e8ac538355
Johannes Schumann <johannes.schumann@fau.de>
2020-08-12 22:03:44 +0200

    Added tests

--- test/oscillation_tests.jl
+++ test/oscillation_tests.jl
@@ -23,3 +23,11 @@ test_values = Neurthino.transprob(U, H, 1, 1.6e4)
 @test test_values[2,2] ≈ 0.330 atol=0.01 
 @test test_values[2,3] ≈ 0.233 atol=0.01 
 @test test_values[3,3] ≈ 0.565 atol=0.01 
+
+test_values = Neurthino.transprob(osc, 1, 1.6e4)
+@test test_values[1,1] ≈ 0.360 atol=0.01 
+@test test_values[1,2] ≈ 0.437 atol=0.01 
+@test test_values[1,3] ≈ 0.202 atol=0.01 
+@test test_values[2,2] ≈ 0.330 atol=0.01 
+@test test_values[2,3] ≈ 0.233 atol=0.01 
+@test test_values[3,3] ≈ 0.565 atol=0.01 
