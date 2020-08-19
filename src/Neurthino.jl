258a5c062beddeca8877195d53af148e1142a573
Tamas Gal <tgal@km3net.de>
2020-08-19 16:41:27 +0200

    Cleanup

--- src/Neurthino.jl
+++ src/Neurthino.jl
@@ -7,7 +7,6 @@ using Unitful
 using Polynomials
 using DocStringExtensions
 
-import Base
 export transprob, OscillationParameters, PMNSMatrix, Hamiltonian, MatterOscillationMatrices
 
 # TEMPORARY UNTIL PhysicalConstants.jl GETS UPDATED
@@ -15,10 +14,7 @@ G_F = 1.1663787e-5u"GeV^-2"
 G_F = G_F * (PhysicalConstants.CODATA2018.SpeedOfLightInVacuum * PhysicalConstants.CODATA2018.ReducedPlanckConstant)^3
 G_F = uconvert(u"eV*cm^3", G_F)
 
-
 include("PREM.jl")
 include("Oscillation.jl")
-
-
 
 end # module
