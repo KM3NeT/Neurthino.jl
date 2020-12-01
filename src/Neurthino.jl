aa9e23df4ec8ceb628edb89e94f58f22ca750bb8
Tamas Gal <tgal@km3net.de>
2020-12-01 18:00:44 +0100

    Const avogadro and gravity

--- src/Neurthino.jl
+++ src/Neurthino.jl
@@ -10,8 +10,8 @@ using Interpolations
 export transprob, OscillationParameters, PMNSMatrix, Hamiltonian, MatterOscillationMatrices
 export masssquareddiff!, cpphase!, mixingangle!
 
-N_A = 6.022e23 #[mol^-1]
-G_F = 8.961877245622253e-38 #[eV*cm^3]
+const N_A = 6.022e23 #[mol^-1]
+const G_F = 8.961877245622253e-38 #[eV*cm^3]
 
 include("PREM.jl")
 include("Oscillation.jl")
