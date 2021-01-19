7cb6fc81687093b255ab3a30baaa296182b6fe58
Johannes Schumann <johannes.schumann@fau.de>
2021-01-19 01:59:39 +0100

    Update path tests

--- test/path_tests.jl
+++ test/path_tests.jl
@@ -13,14 +13,12 @@ import Neurthino
 @test Neurthino.tracklength(2.97, 1860) ≈ 10769.02 atol=1.0 
 @test Neurthino.tracklength(2.27, 5600) ≈ 6839.15 atol=1.0 
 
-distances, densities = Neurthino.prempath(pi, 1.5)
+path = Neurthino.prempath(pi, 1.5)
 
-for d in distances
-    @test distances[1] ≈ 128.69 atol=0.1 
-end
+@test path.baseline[1] ≈ 128.69 atol=0.1 
 
-@test densities[1] ≈ 3.38 atol=0.1
-@test densities[99] ≈ 3.38 atol=0.1
-@test densities[50] ≈ 13.09 atol=0.1
-@test densities[20] ≈ 5.38 atol=0.1
-@test densities[80] ≈ 5.38 atol=0.1
+@test path.density[1] ≈ 3.38 atol=0.1
+@test path.density[99] ≈ 3.38 atol=0.1
+@test path.density[50] ≈ 13.09 atol=0.1
+@test path.density[20] ≈ 5.38 atol=0.1
+@test path.density[80] ≈ 5.38 atol=0.1
