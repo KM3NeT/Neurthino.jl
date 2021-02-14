fb1320733616c16a5962a300744c93175cf13723
Johannes Schumann <johannes.schumann@fau.de>
2021-02-14 02:04:34 +0100

    First matter tests

--- test/matter_tests.jl
+++ test/matter_tests.jl
@@ -13,7 +13,37 @@ test_values = Pνν(U_mat, H_mat, 1, 10000)
 @test test_values[1,1] ≈ 0.834 atol=0.01                                        
 @test test_values[1,2] ≈ 0.083 atol=0.01                                        
 @test test_values[1,3] ≈ 0.083 atol=0.01                                        
+@test test_values[2,1] ≈ 0.108 atol=0.01                                        
 @test test_values[2,2] ≈ 0.052 atol=0.01                                        
 @test test_values[2,3] ≈ 0.840 atol=0.01                                        
+@test test_values[3,1] ≈ 0.058 atol=0.01                                        
+@test test_values[3,2] ≈ 0.865 atol=0.01                                        
 @test test_values[3,3] ≈ 0.077 atol=0.01  
 
+p = Neurthino.Path([13],[10000])
+E = [1,2]
+test_values = Pνν(osc, E, p)
+@test test_values[1,1,1,1] ≈ 0.834 atol=0.01                                        
+@test test_values[1,1,1,2] ≈ 0.083 atol=0.01                                        
+@test test_values[1,1,1,3] ≈ 0.083 atol=0.01                                        
+@test test_values[1,1,2,1] ≈ 0.108 atol=0.01                                        
+@test test_values[1,1,2,2] ≈ 0.052 atol=0.01                                        
+@test test_values[1,1,2,3] ≈ 0.840 atol=0.01                                        
+@test test_values[1,1,3,1] ≈ 0.058 atol=0.01                                        
+@test test_values[1,1,3,2] ≈ 0.865 atol=0.01                                        
+@test test_values[1,1,3,3] ≈ 0.077 atol=0.01  
+
+
+p = Neurthino.Path([13,13,13,13],[2500,2500,2500,2500])
+E = 1
+
+test_values = Pνν(osc, E, p)
+@test test_values[1,1,1,1] ≈ 0.834 atol=0.01                                        
+@test test_values[1,1,1,2] ≈ 0.083 atol=0.01                                        
+@test test_values[1,1,1,3] ≈ 0.083 atol=0.01                                        
+@test test_values[1,1,2,1] ≈ 0.108 atol=0.01                                        
+@test test_values[1,1,2,2] ≈ 0.052 atol=0.01                                        
+@test test_values[1,1,2,3] ≈ 0.840 atol=0.01                                        
+@test test_values[1,1,3,1] ≈ 0.058 atol=0.01                                        
+@test test_values[1,1,3,2] ≈ 0.865 atol=0.01                                        
+@test test_values[1,1,3,3] ≈ 0.077 atol=0.01  
